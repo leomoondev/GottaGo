841d1abdc1c7c2441398e1be768e428c7fe8b26c
Leo Moon <leomoondev@gmail.com>
2017-03-07 15:21:59 -0800

    Fixed the null value showing on the maintainter and location on the storyboard

--- GottaGo/Pin.m
+++ GottaGo/Pin.m
@@ -12,14 +12,13 @@
 
 -(instancetype)initWithName:(NSString *)name andWithAddress:(NSString *)address andWithType:(NSString *)type andWithLocation:(NSString *)location andWithSummerHours:(NSString *)summerHours andWithWinterHours:(NSString *)winterHours andWithWheelchairAccess:(NSString *)wheelchairAccess andWithMaintainer:(NSString *)maintainer andWithLatitude:(double)latitude andWithLongitude:(double)longitude {
 
-
     if (self = [super init]) {
         _name = name;
         _address = address;
         _type = type;
-        //_location = location;
+        _location = location;
         _wheelchairAccess = wheelchairAccess;
-       // _maintainer = maintainer;
+        _maintainer = maintainer;
         _latitude = latitude;
         _longitude = longitude;
         _summerHours = summerHours;
