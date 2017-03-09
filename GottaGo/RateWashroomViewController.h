a754d74b94c84a2688feab71d6a67069b4187787
luckabo <hetland.scott@gmail.com>
2017-03-09 11:48:41 -0800

    add rate washroom vc

---
+++ GottaGo/RateWashroomViewController.h
@@ -0,0 +1,13 @@
+//
+//  RateWashroomViewController.h
+//  GottaGo
+//
+//  Created by Scott Hetland on 2017-03-08.
+//  Copyright © 2017 Scott Hetland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RateWashroomViewController : UIViewController
+
+@end
