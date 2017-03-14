45e6a746b4657c5c6c1924c2ee51b9b3d299a22a
Leo Moon <leomoondev@gmail.com>
2017-03-13 23:59:29 -0700

    Update summer working hours

--- GottaGo/ShowOpenWashrooms.m
+++ GottaGo/ShowOpenWashrooms.m
@@ -255,197 +255,197 @@
         
         // March to September : Show Summer Hours
         else {
-            if ([self.object.winterHours isEqualToString:@"Dawn to Dusk"]) {
+            if ([self.object.summerHours isEqualToString:@"Dawn to Dusk"]) {
                 
-                self.startingHourWinter = 7;
-                self.startingMinutesWinter = 0;
+                self.startingHourSummer = 7;
+                self.startingMinutesSummer = 0;
                 
-                self.closingHourWinter = 19;
-                self.closingMinutesWinter = 0;
+                self.closingHourSummer = 19;
+                self.closingMinutesSummer = 0;
                 
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"24 hrs"]) {
-                self.startingHourWinter = 0;
-                self.startingMinutesWinter = 0;
+            if([self.object.summerHours isEqualToString:@"24 hrs"]) {
+                self.startingHourSummer = 0;
+                self.startingMinutesSummer = 0;
                 
-                self.closingHourWinter = 25;
-                self.closingMinutesWinter = 61;
+                self.closingHourSummer = 25;
+                self.closingMinutesSummer = 61;
                 
                 [self addOpenWashroomsToArray];
             }
-            if([self.object.winterHours isEqualToString:@"10:00 am - Dusk"]) {
-                self.startingHourWinter = 10;
-                self.startingMinutesWinter = 0;
+            if([self.object.summerHours isEqualToString:@"10:00 am - Dusk"]) {
+                self.startingHourSummer = 10;
+                self.startingMinutesSummer = 0;
                 
-                self.closingHourWinter = 19;
-                self.closingMinutesWinter = 0;
+                self.closingHourSummer = 19;
+                self.closingMinutesSummer = 0;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"12:00 pm - 4:00 pm"]) {
-                self.startingHourWinter = 12;
-                self.startingMinutesWinter = 0;
+            if([self.object.summerHours isEqualToString:@"12:00 pm - 4:00 pm"]) {
+                self.startingHourSummer = 12;
+                self.startingMinutesSummer = 0;
                 
-                self.closingHourWinter = 16;
-                self.closingMinutesWinter = 0;
+                self.closingHourSummer = 16;
+                self.closingMinutesSummer = 0;
                 [self addOpenWashroomsToArray];
                 
             }
             
-            if([self.object.winterHours isEqualToString:@"5:30 am - Dusk"]) {
-                self.startingHourWinter = 5;
-                self.startingMinutesWinter = 30;
+            if([self.object.summerHours isEqualToString:@"5:30 am - Dusk"]) {
+                self.startingHourSummer = 5;
+                self.startingMinutesSummer = 30;
                 
-                self.closingHourWinter = 19;
-                self.closingMinutesWinter = 0;
+                self.closingHourSummer = 19;
+                self.closingMinutesSummer = 0;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"6:00 am - 8:30 pm"]) {
-                self.startingHourWinter = 6;
-                self.startingMinutesWinter = 0;
+            if([self.object.summerHours isEqualToString:@"6:00 am - 8:30 pm"]) {
+                self.startingHourSummer = 6;
+                self.startingMinutesSummer = 0;
                 
-                self.closingHourWinter = 20;
-                self.closingMinutesWinter = 30;
+                self.closingHourSummer = 20;
+                self.closingMinutesSummer = 30;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"7:00 am - Dusk"]) {
-                self.startingHourWinter = 7;
-                self.startingMinutesWinter = 0;
+            if([self.object.summerHours isEqualToString:@"7:00 am - Dusk"]) {
+                self.startingHourSummer = 7;
+                self.startingMinutesSummer = 0;
                 
-                self.closingHourWinter = 19;
-                self.closingMinutesWinter = 0;
+                self.closingHourSummer = 19;
+                self.closingMinutesSummer = 0;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"7:30 am - 9:00 pm"]) {
-                self.startingHourWinter = 7;
-                self.startingMinutesWinter = 30;
+            if([self.object.summerHours isEqualToString:@"7:30 am - 9:00 pm"]) {
+                self.startingHourSummer = 7;
+                self.startingMinutesSummer = 30;
                 
-                self.closingHourWinter = 21;
-                self.closingMinutesWinter = 0;
+                self.closingHourSummer = 21;
+                self.closingMinutesSummer = 0;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"7:30 am - Dusk"]) {
-                self.startingHourWinter = 7;
-                self.startingMinutesWinter = 30;
+            if([self.object.summerHours isEqualToString:@"7:30 am - Dusk"]) {
+                self.startingHourSummer = 7;
+                self.startingMinutesSummer = 30;
                 
-                self.closingHourWinter = 19;
-                self.closingMinutesWinter = 0;
+                self.closingHourSummer = 19;
+                self.closingMinutesSummer = 0;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"9:00 am - Dusk"]) {
-                self.startingHourWinter = 9;
-                self.startingMinutesWinter = 0;
+            if([self.object.summerHours isEqualToString:@"9:00 am - Dusk"]) {
+                self.startingHourSummer = 9;
+                self.startingMinutesSummer = 0;
                 
-                self.closingHourWinter = 19;
-                self.closingMinutesWinter = 0;
+                self.closingHourSummer = 19;
+                self.closingMinutesSummer = 0;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"Closed"]) {
-                self.startingHourWinter = 25;
-                self.startingMinutesWinter = 99;
+            if([self.object.summerHours isEqualToString:@"Closed"]) {
+                self.startingHourSummer = 25;
+                self.startingMinutesSummer = 99;
                 
-                self.closingHourWinter = 25;
-                self.closingMinutesWinter = 99;
+                self.closingHourSummer = 25;
+                self.closingMinutesSummer = 99;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"Dawn - 11:00 pm"]) {
-                self.startingHourWinter = 07;
-                self.startingMinutesWinter = 0;
+            if([self.object.summerHours isEqualToString:@"Dawn - 11:00 pm"]) {
+                self.startingHourSummer = 07;
+                self.startingMinutesSummer = 0;
                 
-                self.closingHourWinter = 23;
-                self.closingMinutesWinter = 0;
+                self.closingHourSummer = 23;
+                self.closingMinutesSummer = 0;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"Dawn to 10:15 pm"]) {
-                self.startingHourWinter = 7;
-                self.startingMinutesWinter = 0;
+            if([self.object.summerHours isEqualToString:@"Dawn to 10:15 pm"]) {
+                self.startingHourSummer = 7;
+                self.startingMinutesSummer = 0;
                 
-                self.closingHourWinter = 22;
-                self.closingMinutesWinter = 15;
+                self.closingHourSummer = 22;
+                self.closingMinutesSummer = 15;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"Dawn to 11:15 pm"]) {
-                self.startingHourWinter = 7;
-                self.startingMinutesWinter = 0;
+            if([self.object.summerHours isEqualToString:@"Dawn to 11:15 pm"]) {
+                self.startingHourSummer = 7;
+                self.startingMinutesSummer = 0;
                 
-                self.closingHourWinter = 23;
-                self.closingMinutesWinter = 15;
+                self.closingHourSummer = 23;
+                self.closingMinutesSummer = 15;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"Dawn to Dusk Weekends only"]) {
-                self.startingHourWinter = 7;
-                self.startingMinutesWinter = 0;
+            if([self.object.summerHours isEqualToString:@"Dawn to Dusk Weekends only"]) {
+                self.startingHourSummer = 7;
+                self.startingMinutesSummer = 0;
                 
-                self.closingHourWinter = 19;
-                self.closingMinutesWinter = 00;
+                self.closingHourSummer = 19;
+                self.closingMinutesSummer = 00;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"Hours of operation only"]) {
-                self.startingHourWinter = 25;
-                self.startingMinutesWinter = 99;
+            if([self.object.summerHours isEqualToString:@"Hours of operation only"]) {
+                self.startingHourSummer = 25;
+                self.startingMinutesSummer = 99;
                 
-                self.closingHourWinter = 25;
-                self.closingMinutesWinter = 99;
+                self.closingHourSummer = 25;
+                self.closingMinutesSummer = 99;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"Hours of the centre"]) {
-                self.startingHourWinter = 25;
-                self.startingMinutesWinter = 99;
+            if([self.object.summerHours isEqualToString:@"Hours of the centre"]) {
+                self.startingHourSummer = 25;
+                self.startingMinutesSummer = 99;
                 
-                self.closingHourWinter = 25;
-                self.closingMinutesWinter = 99;
+                self.closingHourSummer = 25;
+                self.closingMinutesSummer = 99;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours containsString:@"Men"]) {
-                self.startingHourWinter = 8;
-                self.startingMinutesWinter = 0;
+            if([self.object.summerHours containsString:@"Men"]) {
+                self.startingHourSummer = 8;
+                self.startingMinutesSummer = 0;
                 
-                self.closingHourWinter = 23;
-                self.closingMinutesWinter = 59;
-                [self addOpenWashroomsToArray];
-                
-                
-            }
-            if([self.object.winterHours isEqualToString:@"Mon - Fri 7:00 am - 3:30 pm"]) {
-                self.startingHourWinter = 7;
-                self.startingMinutesWinter = 0;
-                
-                self.closingHourWinter = 15;
-                self.closingMinutesWinter = 30;
+                self.closingHourSummer = 23;
+                self.closingMinutesSummer = 59;
                 [self addOpenWashroomsToArray];
                 
                 
             }
-            if([self.object.winterHours isEqualToString:@"Tue - Sat 9:00 am - 8:00 pm"]) {
-                self.startingHourWinter = 9;
-                self.startingMinutesWinter = 0;
+            if([self.object.summerHours isEqualToString:@"Mon - Fri 7:00 am - 3:30 pm"]) {
+                self.startingHourSummer = 7;
+                self.startingMinutesSummer = 0;
                 
-                self.closingHourWinter = 20;
-                self.closingMinutesWinter = 0;
+                self.closingHourSummer = 15;
+                self.closingMinutesSummer = 30;
+                [self addOpenWashroomsToArray];
+                
+                
+            }
+            if([self.object.summerHours isEqualToString:@"Tue - Sat 9:00 am - 8:00 pm"]) {
+                self.startingHourSummer = 9;
+                self.startingMinutesSummer = 0;
+                
+                self.closingHourSummer = 20;
+                self.closingMinutesSummer = 0;
                 [self addOpenWashroomsToArray];
                 
             }
-            if([self.object.winterHours isEqualToString:@"Weekdays only 8:30 am - 5:00 pm"]) {
-                self.startingHourWinter = 8;
-                self.startingMinutesWinter = 30;
+            if([self.object.summerHours isEqualToString:@"Weekdays only 8:30 am - 5:00 pm"]) {
+                self.startingHourSummer = 8;
+                self.startingMinutesSummer = 30;
                 
-                self.closingHourWinter = 17;
-                self.closingMinutesWinter = 0;
+                self.closingHourSummer = 17;
+                self.closingMinutesSummer = 0;
                 [self addOpenWashroomsToArray];
                 
             }
@@ -474,6 +474,7 @@
 }
 
 - (void) addOpenWashroomsToArray {
+    
     [self checkOpeningFirst:self.startingHourWinter second:self.startingMinutesWinter third:self.closingHourWinter fourth:self.closingMinutesWinter];
     if(self.isWashroomOpen == true) {
         
